cd8bfba28fbc661d1c005cfd41280f87b9ef70cb
Heis40 <isaacheisdorffer467@gmail.com>
2025-08-23 13:41:24 -0500

    Isaac Heisdorffer 8/23/2025, Added intorduce method and overlaoded greet method.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,14 +1,41 @@
 public class HelloWorld {
       
     String name; 
+    String message;
+    private int age;
+    
+
     public HelloWorld(String name) { 
     this.name = name; 
     } 
-    public void greet() { 
-    System.out.println("Hello, " + name + "!"); 
+
+    public HelloWorld(String name, int age, String message)
+    {
+     this.name = name;
+     this.age = age; 
+     this.message = message;
+    }
+
+    //add Introduce method
+    public void introduce()
+    {
+        System.out.println("My name is " + name + " and I am " + age + " years old.");
+    }
+    //overload greet method
+    public void greet() 
+    { 
+        System.out.println("Hello, " + name + "!"); 
     } 
     
+    public void greet(String message) 
+    { 
+        System.out.println(message + ", " + name + "!"); 
+    }
+
     public static void main(String[] args) { 
-        HelloWorld student = new HelloWorld("Isaac"); student.greet(); 
+        HelloWorld student = new HelloWorld("Isaac",21,"Good morning");  
+        student.introduce();
+        student.greet(); 
+        student.greet("Good morning");
     } 
 }
